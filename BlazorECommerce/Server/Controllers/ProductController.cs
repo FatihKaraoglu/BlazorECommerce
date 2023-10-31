70066da39e18588ef054327e7eaf2900989ee893
FatihKaraoglu <karaoglu.fatih16@gmail.com>
2023-10-31 10:20:17 +0100

    Projektdateien hinzufügen.

---
+++ BlazorECommerce/Server/Controllers/ProductController.cs
@@ -0,0 +1,24 @@
+﻿using BlazorECommerce.Server.Data;
+using BlazorECommerce.Shared;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlazorECommerce.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly DataContext _context;
+        public ProductController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet] public async Task<ActionResult<List<Product>>> GetProdcut() 
+        {
+            var products = await _context.Products.ToListAsync();
+            return Ok(products);
+        }
+    }
+}
